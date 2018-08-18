248fa5fb2b8b1394fc3fddf5107177e7958408a7
wbruey <willbruey@gmail.com>
2018-08-18 11:13:00 -0400

    im adding some basic files for html and js interaction.  this way i can build a CRUD microservice per my evernotenotes.  this code doesn't do much besides a hello world interaction between html and javascript using the <script> syntax

---
+++ html-js/helloWorld.js
@@ -0,0 +1,9 @@
+let a = 10;
+console.log(this);
+console.log("Hello Bruester");
+function print(name){
+	document.write(name);
+}
+print("bruester baby\n\r");
+
+return '<p> This is my first web page. <br /> I hope you like it. </p>'
